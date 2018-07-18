6f835ddb881a0d4d80cde76d4985e402e73d0bc8
Manohar Mukku <manoharmukku@gmail.com>
2018-07-18 15:42:57 +0530

    Update main.c

--- main.c
+++ main.c
@@ -8,82 +8,81 @@ GitHub: https://github.com/manoharmukku/multilayer-perceptron-in-c
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-Hidden Layer sizes: # of hidden layers, Size of each hidden layer separated by space (int)
-Hidden activation function: identity, sigmoid, tanh, relu (char*)
-Alpha (L2 Regularization parameter value): Float value (float)
-Maximum number of iterations: > 0 (int)
-Momentum for gradient descent update: Integer value (int)
-Number of units in output layer: # of units in output layer (int)
-Output activation: identity, sigmoid, tanh, relu (char*)
-Filename: Name of the csv file containing the dataset
-Rows: Number of rows or samples in the dataset
-Cols: Number of features including the output variable in the dataset
-*/
-
 typedef struct {
     int n_hidden;
-    int* hidden_layer_size;
+    int* hidden_layer_sizes;
     int hidden_activation_function;
     float regularization_parameter;
     int n_iterations_max;
     int momentum;
     int output_layer_size;
     int output_activation_function;
-} training_parameters;
-
-typedef struct {
-    char* filename;
     int sample_size;
     int feature_size;
-} data_parameters;
+    double** data;
+} parameters;
 
 int main(int argc, char** argv) {
+    /*
+    argv[0]: Executable file name Ex: a.out
+    argv[1]: Number of hidden layers Ex: 3
+    argv[2]: Size of each hidden layer separated by comma Ex: 4,5,5
+    argv[3]: Hidden activation function (identity - 1, sigmoid - 2, tanh - 3, relu - 4)
+    argv[4]: Alpha (L2 Regularization parameter value)
+    argv[5]: Maximum number of iterations
+    argv[6]: Momentum for gradient descent update
+    argv[7]: Number of units in output layer
+    argv[8]: Output activation function (identity - 1, sigmoid - 2, tanh - 3, relu - 4)
+    argv[9]: Name of the csv file containing the dataset
+    argv[10]: Number of rows or samples in the dataset
+    argv[11]: Number of features including the output variable in the dataset
+    */
+
     // Sanity check of command line arguments
-    if (argc < 1) {
-        printf("Usage: 'No. of hidden layers' 'Size of each hidden layer separated by space' 'Hidden activation'\n
+    if (argc <= 1) {
+        printf("Usage: ./a.out 'No. of hidden layers' 'Size of each hidden layer separated by comma' 'Hidden activation'\n
             'Alpha' 'Max iterations' 'Momentum' 'Size of output layer' 'Output activation' 'Filename' 'Rows' 'Cols'\n");
         exit(0);
     }
 
     // Create memory for training parameters struct
-    training_parameters* train_params = (training_parameters*)malloc(sizeof(training_parameters));
+    parameters* params = (parameters*)malloc(sizeof(parameters));
 
     // Number of hidden layers
-    train_params->n_hidden = atoi(argv[1]);
+    params->n_hidden = atoi(argv[1]);
     // Sanity check of number of hidden layers
-    if (train_params->n_hidden < 0) {
+    if (params->n_hidden < 0) {
         printf("Error: Number of hidden layers should be >= 0\n");
         exit(0);
     }
 
     // Size of each hidden layer
-    train_params->hidden_layer_size = (int*)malloc(train_params->n_hidden * sizeof(int));
+    params->hidden_layer_sizes = (int*)malloc(params->n_hidden * sizeof(int));
     int i;
-    for (i = 0; i < train_params->n_hidden; i++) {
-        train_params->hidden_layer_size[i] = atoi(argv[i+2]);
-
+    char* tok;
+    for (i = 0, tok = strtok(argv[2], ","); tok = strtok(NULL, ",") && i < params->n_hidden; i++) {
+        params->hidden_layer_sizes[i] = atoi(tok);
         // Sanity check of size of hidden layer
-        if (train_params->hidden_layer_size[i] <= 0) {
+        if (params->hidden_layer_sizes[i] <= 0) {
             printf("Error: Hidden layer sizes should be positive\n");
             exit(0);
         }
     }
 
     // Hidden activation function
-    train_params->hidden_activation_function;
-    switch (argv[train_params->n_hidden + 2]) {
+    params->hidden_activation_function;
+    switch (argv[3]) {
         case "identity":
-            train_params->hidden_activation_function = 1;
+            params->hidden_activation_function = 1;
             break;
         case "sigmoid":
-            train_params->hidden_activation_function = 2;
+            params->hidden_activation_function = 2;
             break;
         case "tanh":
-            train_params->hidden_activation_function = 3;
+            params->hidden_activation_function = 3;
             break;
         case "relu":
-            train_params->hidden_activation_function = 4;
+            params->hidden_activation_function = 4;
             break;
         default:
             printf("Error: Invalid value for hidden activation function\n");
@@ -93,39 +92,39 @@ int main(int argc, char** argv) {
     }
 
     // L2 Regularization parameter
-    train_params->regularization_parameter = atoi(argv[train_params->n_hidden + 3]);
+    params->regularization_parameter = atoi(argv[4]);
 
     // Max. number of iterations
-    train_params->n_iterations_max = atoi(argv[train_params->n_hidden + 4]);
-    if (train_params->n_iterations_max <= 0) {
+    params->n_iterations_max = atoi(argv[5]);
+    if (params->n_iterations_max <= 0) {
         printf("Max. number of iterations value should be positive\n");
         exit(0);
     }
 
     // Momentum
-    train_params->momentum = atoi(argv[train_params->n_hidden + 5]);
+    params->momentum = atoi(argv[6]);
 
     // Output layer size
-    train_params->output_layer_size = atoi(argv[train_params->n_hidden + 6]);
-    if (train_params->output_layer_size <= 0) {
+    params->output_layer_size = atoi(argv[7]);
+    if (params->output_layer_size <= 0) {
         printf("Output layer size should be positive\n");
         exit(0);
     }
 
     // Output activation function
-    train_params->output_activation_function;
-    switch (argv[train_params->n_hidden + 7]) {
+    params->output_activation_function;
+    switch (argv[8]) {
         case "identity":
-            train_params->output_activation_function = 1;
+            params->output_activation_function = 1;
             break;
         case "sigmoid":
-            train_params->output_activation_function = 2;
+            params->output_activation_function = 2;
             break;
         case "tanh":
-            train_params->output_activation_function = 3;
+            params->output_activation_function = 3;
             break;
         case "relu":
-            train_params->output_activation_function = 4;
+            params->output_activation_function = 4;
             break;
         default:
             printf("Error: Invalid value for output activation function\n");
@@ -134,13 +133,28 @@ int main(int argc, char** argv) {
             break;
     }
 
-    // Create memory for the data parameters struct
-    data_parameters* data_params = (data_parameters*)malloc(sizeof(data_parameters));
-
     // Get the parameters of the dataset
-    data_params->filename = argv[train_params->n_hidden + 8];
-    data_params->sample_size = atoi(argv[train_params->n_hidden + 9]);
-    data_params->feature_size = atoi(argv[train_params->n_hidden + 10]);
+    char* filename = argv[9];
+    params->sample_size = atoi(argv[10]);
+    params->feature_size = atoi(argv[11]);
+
+    // Create 2D array memory for the dataset
+    params->data = (double**)malloc(params->sample_size * sizeof(double*));
+    int i;
+    for (i = 0; i < params->sample_size; i++)
+        params->data[i] = (double*)malloc(params->feature_size * sizeof(double));
+
+    // Read the dataset from the csv into the 2D array
+    read_csv(filename, params->sample_size, params->feature_size, data);
+
+    
+
+    // Free the allocated memory
+    for (i = 0; i < params->sample_size; i++)
+        free(params->data[i]);
+    free(params->data);
+    free(params->hidden_layer_sizes);
+    free(params);
 
     return 0;
 }
