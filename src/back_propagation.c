41134349e8d89ad88dd8866e7e869352ca5c826a
Manohar Mukku <manoharmukku@gmail.com>
2018-07-25 14:21:07 +0530

    Added code for memory for local gradient

--- src/back_propagation.c
+++ src/back_propagation.c
@@ -20,10 +20,11 @@ typedef struct {
 } parameters;
 
 void back_propagation(parameters* param, int training_example, int n_layers, int** layer_sizes, double** layer_inputs, double** layer_outputs, double*** weight) {
+    /* ------------------ Expected output ----------------------------------------*/
     // Get the expected output from the data matrix
     // Create memory for the expected output array
     // Initialized to zero's
-    double* expected_output = (double*)calloc(output_layer_size, sizeof(double));
+    double* expected_output = (double*)calloc(param->output_layer_size, sizeof(double));
 
     // Make the respective element in expected_output to 1 and rest all 0
     // Ex: If y = 3 and output_layer_size = 4 then expected_output = [0, 0, 1, 0]
@@ -32,12 +33,14 @@ void back_propagation(parameters* param, int training_example, int n_layers, int
     else 
         expected_output[param->data[training_example][param->feature_size-1] - 1] = 1;
 
+    /* ---------------------- Weight correction ----------------------------------- */
     // Create memory for the weight_correction matrices between layers
     // weight_correction is a pointer to the array of 2D arrays between the layers
     double*** weight_correction = (double***)calloc(n_layers - 1, sizeof(double**));
 
     // Each 2D array between two layers i and i+1 is of size ((layer_size[i]+1) x layer_size[i+1])
     // The weight_correction matrix includes weight corrections for the bias terms too
+    int i;
     for (i = 0; i < n_layers-1; i++)
         weight_correction[i] = (double**)calloc(layer_sizes[i]+1, sizeof(double*));
 
@@ -46,9 +49,23 @@ void back_propagation(parameters* param, int training_example, int n_layers, int
         for (j = 0; j < layer_sizes[i]+1; j++)
             weight_correction[i][j] = (double*)calloc(layer_sizes[i+1], sizeof(double));
 
+    /* --------------------- Local Gradient --------------------------------------*/
+    // Create memory for local gradient (delta) for each layer
+    double** local_gradient = (double**)calloc(n_layers, sizeof(double*));
+
+    for (i = 0; i < n_layers; i++)
+        local_gradient[i] = (double*)calloc(layer_sizes[i], sizeof(double));
+
+    /*----------- Calculate weight corrections for all weights -------------------*/
+    // Weight correction for the output layer
 
 
     // Free the memory allocated in Heap
+    for (i = 0; i < n_layers; i++)
+        free(local_gradient[i]);
+
+    free(local_gradient);
+
     for (i = 0; i < n_layers - 1; i++)
         for (j = 0; j < layer_sizes[i]+1; j++)
             free(weight_correction[i][j]);
