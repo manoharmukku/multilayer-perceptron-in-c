3a03ba49e1f3808a063a760df262f13858e4fe8c
Manohar Mukku <manoharmukku@gmail.com>
2018-07-25 15:31:10 +0530

    Added code for calculating local gradient for output layer

--- src/back_propagation.c
+++ src/back_propagation.c
@@ -5,6 +5,8 @@ Desc: Backpropagation in C
 GitHub: https://github.com/manoharmukku/multilayer-perceptron-in-c
 */
 
+#include <stdlib.h>
+
 typedef struct {
     int n_hidden;
     int* hidden_layers_size;
@@ -28,13 +30,13 @@ void d_identity(int layer_size, double* layer_input, double* layer_output, doubl
 void d_sigmoid(int layer_size, double* layer_input, double* layer_output, double* layer_derivative) {
     int i;
     for (i = 0; i < layer_size; i++)
-        layer_derivatie[i] = layer_output[i+1] * (1.0 - layer_output[i+1]);
+        layer_derivative[i] = layer_output[i+1] * (1.0 - layer_output[i+1]);
 }
 
 void d_tanh(int layer_size, double* layer_input, double* layer_output, double* layer_derivative) {
     int i;
     for (i = 0; i < layer_size; i++)
-        layer_derivatie[i] = 1.0 - layer_output[i+1] * layer_output[i+1];
+        layer_derivative[i] = 1.0 - layer_output[i+1] * layer_output[i+1];
 }
 
 void d_relu(int layer_size, double* layer_input, double* layer_output, double* layer_derivative) {
@@ -45,14 +47,84 @@ void d_relu(int layer_size, double* layer_input, double* layer_output, double* l
         else if (layer_input[i] < 0)
             layer_derivative[i] = 0;
         else // derivative does not exist
-            layer_derivatie[i] = 0.5; // giving arbitrary value
+            layer_derivative[i] = 0.5; // giving arbitrary value
     }
 }
 
 void d_softmax(int layer_size, double* layer_input, double* layer_output, double* layer_derivative) {
     int i;
     for (i = 0; i < layer_size; i++)
-        layer_derivatie[i] = layer_output[i+1] * (1.0 - layer_output[i+1]);
+        layer_derivative[i] = layer_output[i+1] * (1.0 - layer_output[i+1]);
+}
+
+void calculate_local_gradient(int isOutputLayer, int n_layers, int** layer_sizes, double** layer_inputs, double** layer_outputs, 
+    double** weight, double* expected_output, double** local_gradient) {
+    if (isOutputLayer) {
+        // Error produced at the output layer
+        double* error_output = (double*)calloc(param->output_layer_size, sizeof(double));
+
+        int i;
+        for (i = 0; i < output_layer_size; i++)
+            error_output[i] = expected_output[i] - layer_outputs[n_layers-1][i+1];
+
+        // Create memory for derivatives
+        double** layer_derivatives = (double**)calloc(n_layers, sizeof(double*));
+
+        for (i = 0; i < n_layers; i++)
+            layer_derivatives[i] = (double*)calloc(layer_sizes[i], sizeof(double));
+
+        // Calculate the layer derivatives
+        // Calculate the local gradients
+        swith(param->output_activation_function) {
+            case 1: // identity
+                d_identity(output_layer_size, layer_inputs[n_layers-1], layer_outputs[n_layers-1], layer_derivatives[n_layers-1]);
+
+                for (i = 0; i < output_layer_size; i++)
+                    local_gradient[n_layers-1][i] = error_output[i] * layer_derivatives[n_layers-1][i];
+
+                break;
+            case 2: // sigmoid
+                d_sigmoid(output_layer_size, layer_inputs[n_layers-1], layer_outputs[n_layers-1], layer_derivatives[n_layers-1]);
+
+                for (i = 0; i < output_layer_size; i++)
+                    local_gradient[n_layers-1][i] = error_output[i] * layer_derivatives[n_layers-1][i];
+
+                break;
+            case 3: // tanh
+                d_tanh(output_layer_size, layer_inputs[n_layers-1], layer_outputs[n_layers-1], layer_derivatives[n_layers-1]);
+
+                for (i = 0; i < output_layer_size; i++)
+                    local_gradient[n_layers-1][i] = error_output[i] * layer_derivatives[n_layers-1][i];
+
+                break;
+            case 4: // relu
+                d_relu(output_layer_size, layer_inputs[n_layers-1], layer_outputs[n_layers-1], layer_derivatives[n_layers-1]);
+
+                for (i = 0; i < output_layer_size; i++)
+                    local_gradient[n_layers-1][i] = error_output[i] * layer_derivatives[n_layers-1][i];
+
+                break;
+            case 5: // softmax
+                d_softmax(output_layer_size, layer_inputs[n_layers-1], layer_outputs[n_layers-1], layer_derivatives[n_layers-1]);
+
+                for (i = 0; i < output_layer_size; i++)
+                    local_gradient[n_layers-1][i] = error_output[i] * layer_derivatives[n_layers-1][i];
+
+                break;
+            default:
+                printf("Invalid output activation function\n");
+                exit(0);
+                break;
+        }
+
+        // Free the memory allocated in Heap
+        for (i = 0; i < n_layers; i++)
+            free(layer_derivatives[i]);
+
+        free(layer_derivatives);
+
+        free(error_output);
+    }
 }
 
 void back_propagation(parameters* param, int training_example, int n_layers, int** layer_sizes, double** layer_inputs, double** layer_outputs, double*** weight) {
@@ -94,7 +166,7 @@ void back_propagation(parameters* param, int training_example, int n_layers, int
 
     /*----------- Calculate weight corrections for all weights -------------------*/
     // Weight correction for the output layer
-
+    
 
     // Free the memory allocated in Heap
     for (i = 0; i < n_layers; i++)
