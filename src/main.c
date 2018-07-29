8e8536b37137503149e53531085db46364537e29
Manohar Mukku <manoharmukku@gmail.com>
2018-07-29 10:59:17 +0530

    Updated header

--- src/main.c
+++ src/main.c
@@ -5,24 +5,8 @@ Desc: Multilayer Perceptron implementation in C
 GitHub: https://github.com/manoharmukku/multilayer-perceptron-in-c
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "mlp_trainer.h"
-
-typedef struct {
-    int n_hidden;
-    int* hidden_layers_size;
-    int* hidden_activation_functions;
-    double learning_rate;
-    int n_iterations_max;
-    int momentum;
-    int output_layer_size;
-    int output_activation_function;
-    int sample_size;
-    int feature_size;
-    double** data;
-} parameters;
+#include "parameters.h"
 
 int main(int argc, char** argv) {
     /*
