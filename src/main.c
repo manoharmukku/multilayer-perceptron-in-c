66e65ca3f5fcb85bb745c50fdf7dbafec47986fa
Manohar Mukku <manoharmukku@gmail.com>
2018-08-01 16:36:10 +0530

    Minor changes:

--- src/main.c
+++ src/main.c
@@ -28,7 +28,7 @@ int main(int argc, char** argv) {
     */
 
     // Sanity check of command line arguments
-    if (argc <= 1) {
+    if (argc != 14) {
         // Print help for execution syntax
         printf("\nExecution syntax:\n");
         printf("-----------------\n");
