20cccea144343e48b3f716f3b806d3f53a74b167
Manohar Mukku <manoharmukku@gmail.com>
2018-08-16 20:34:16 +0530

    Added code for confusion matrix for multi class

--- src/mlp_classifier.c
+++ src/mlp_classifier.c
@@ -179,37 +179,85 @@ void mlp_classifier(parameters* param, int* layer_sizes) {
                     max_class = i+1;
                 }
             }
+            final_output[test_example][0] = max_class;
         }
     }
 
     // Calculate the confusion matrix
-    int true_positive = 0, true_negative = 0, false_positive = 0, false_negative = 0;
-    for (test_example = 0; test_example < param->test_sample_size; test_example++) {
-        if (final_output[test_example][0] == 0) {
-            if (param->data_test[test_example][param->feature_size-1] == 0)
-                ++true_negative;
-            else
-                ++false_positive;
+    if (param->output_layer_size == 1) { // Binary classification
+        int true_positive = 0, true_negative = 0, false_positive = 0, false_negative = 0;
+        for (test_example = 0; test_example < param->test_sample_size; test_example++) {
+            if (final_output[test_example][0] == 0) {
+                if (param->data_test[test_example][param->feature_size-1] == 0)
+                    ++true_negative;
+                else
+                    ++false_positive;
+            }
+            else {
+                if (param->data_test[test_example][param->feature_size-1] == 1)
+                    ++true_positive;
+                else
+                    ++false_negative;
+            }
         }
-        else {
-            if (param->data_test[test_example][param->feature_size-1] == 1)
-                ++true_positive;
-            else
-                ++false_negative;
-        }
+
+        // Find the accuracy
+        double accuracy = (double)(true_positive + true_negative) / param->test_sample_size;
+
+        // Print confusion matrix
+        printf("\n\nConfusion matrix\n");
+        printf("-----------------\n\n");
+
+        printf("\t    |predicted 0\t predicted 1\n");
+        printf("--------------------------------------------\n");
+        printf("Actual 0    |%d\t\t%d\n\n", true_negative, false_positive);
+        printf("Actual 1    |%d\t\t%d\n\n", false_negative, true_positive);
+
+        // Print the accuracy
+        printf("\nAccuracy: %.2lf\n\n", accuracy * 100);
     }
-    double accuracy = (double)(true_positive + true_negative) / param->test_sample_size;
+    else { // Multi-class classification
+        int** confusion_matrix = (int**)calloc(param->output_layer_size, sizeof(int*));
+        for (i = 0; i < param->output_layer_size; i++)
+            confusion_matrix[i] = (int*)calloc(param->output_layer_size, sizeof(int));
 
-    // Print confusion matrix
-    printf ("\n\nConfusion matrix\n");
-    printf ("-----------------\n\n");
+        // Fill the confusion matrix
+        int actual_class, predicted_class;
+        for (test_example = 0; test_example < param->test_sample_size; test_example++) {
+            actual_class = param->data_test[test_example][param->feature_size-1] - 1;
+            predicted_class = final_output[test_example][0] - 1;
 
-    printf ("\t    |predicted 0\t predicted 1\n");
-    printf ("--------------------------------------------\n");
-    printf ("Actual 0    |%d\t\t%d\n\n", true_negative, false_positive);
-    printf ("Actual 1    |%d\t\t%d\n\n", false_negative, true_positive);
+            ++confusion_matrix[actual_class][predicted_class];
+        }
 
-    printf ("\nAccuracy: %.2lf\n\n", accuracy * 100);
+        // Print the confusion matrix
+        printf("\t");
+        for (predicted_class = 1; predicted_class <= param->output_layer_size; predicted_class++)
+            printf("Predicted %d  ", predicted_class);
+        printf("\n---------------------------------------------------------------------------\n");
+
+        for (actual_class = 0; actual_class < param->output_layer_size; actual_class++) {
+            printf("Actual %d | ", actual_class+1);
+            for (predicted_class = 0; predicted_class < param->output_layer_size; predicted_class++)
+                printf("%d\t", confusion_matrix[actual_class][predicted_class]);
+            printf("\n");
+        }
+
+        // Find the accuracy
+        double accuracy = 0.0;
+        for (i = 0; i < param->output_layer_size; i++)
+            accuracy += confusion_matrix[i][i];
+        accuracy /= param->test_sample_size;
+
+        // Print the accuracy
+        printf("\nAccuracy: %.2lf\n\n", accuracy * 100);
+
+        // Free the memory allocated in heap
+        for (i = 0; i < param->output_layer_size; i++)
+            free(confusion_matrix[i]);
+        free(confusion_matrix);
+    }
+
 
     // Write the final output into a csv file
     char* output_file_name = "data/data_test_output.csv";
